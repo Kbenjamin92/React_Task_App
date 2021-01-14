d5eb52f38059182352fe71d046fd247cec95c942
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-01-13 19:30:51 -0500

    working on delete button

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const Form = () => {
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
-    const [id, setId] = useState(1)
+    const [id, setId] = useState(0)
     const [dataOutput, setDataOutput] = useState([])
     
 
@@ -17,7 +17,7 @@ const Form = () => {
         }
         // increment the id for each object
         setId(
-            id + 1
+             id + 1 
         )
         // append the new object to the array in state
         setDataOutput([...dataOutput, dataObj])
@@ -26,23 +26,47 @@ const Form = () => {
         setDescription('')
     }
     console.log(dataOutput)
+         // remove object from array in state
+        const removeItem = (itemID) => {
+            for (let i = 0; i < dataOutput.length; i++) {
+                let indexOfArray = dataOutput.indexOf([i])
+                console.log(i)
+    
+                if (id.value !== itemID.value) {
+                   let newArray = dataOutput.splice(indexOfArray)
+                   setDataOutput(newArray)
+                }
+            }
+           
+        }
+
+    
 
     // loop through the array to render on the screen
     const mapData = dataOutput.length !== 0 ? dataOutput.map((obj, i) => {
-        dataOutput.filter(deleteItem)
-        // figure out logic to delete the certian objects
-
+        console.log(id)
+// FIX THIS BUG MAP AND DELETE ITEM 
+   
+    const mapItem =dataOutput.length !== 0 && dataOutput[i] === obj.id ? dataOutput.map((item, i) => {
         return (
             <div key={i} className="map-container">
                 <div className="content-container">
-                    <h3>{obj.id}: {obj.title}</h3>
+                    <h3>{obj.id} {obj.title}</h3>
                     <h3>{obj.description}</h3>
-                    {/* work on button */}
-                    <button className="delBtn" onClick={() => }>Remove</button>
+                    {/* work on  delete button */}
+                    <button className="delBtn" onClick={() => removeItem(obj.id)}>Remove</button>
                 </div>
+    
+            </div>
+            )
+        }) : null
+
+        return (
+            <div>
 
             </div>
         )
+
     }) : null
 
     return (
