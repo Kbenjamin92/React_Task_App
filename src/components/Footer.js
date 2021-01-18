2b1d321fca1671f11e2e517acbedf71f565e272c
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-01-18 07:36:00 -0500

    change github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
         <footer className='footer-container'>
             <h3 className='footer-txt'>Thank you for using my task app!</h3>
             <p>&#169; {year} Kipp Benjamin</p>
-            <a href="https://github.com/Kbenjamin92" target="_blank" rel="noopener noreferrer"><img src={github} className="icon" alt="github"/></a>
+            <a href="https://github.com/Kbenjamin92/React_Task_App" target="_blank" rel="noopener noreferrer"><img src={github} className="icon" alt="github"/></a>
             <a href="https://www.linkedin.com/in/kipp-benjamin-524681155/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className="icon" alt="linkedin" /></a>
             <a href="https://twitter.com/benjamin_kipp" target="_blank" rel="noopener noreferrer"><img src={twitter} className="icon" alt="linkedin" /></a>
 
